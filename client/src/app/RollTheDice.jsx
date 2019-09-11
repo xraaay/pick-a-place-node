521dbe1b32cf2001a96d48b3a9adb8cee9eb45bf
Ray <raymond.y.xie@gmail.com>
2019-09-10 23:56:26 -0700

    front end basic

--- client/src/app/RollTheDice.jsx
+++ client/src/app/RollTheDice.jsx
@@ -1,18 +1,47 @@
 import React, { Component } from 'react'
 import * as yelpService from './services/YelpService'
+import swal from "sweetalert2"
 
 class RollTheDice extends Component {
     componentDidMount(){
-        let data = {
+        this.getGeoLocation()
+    }
 
+    rtd(position){
+        let data = {
+            longitude: position.coords.longitude,
+            latitude: position.coords.latitude,
+            term: "restaurant",
+            radius: 8046,
+            limit: 50,
+            open_now: true,
+            price: 1
         }
-        yelpService.getBusinesses()
+        yelpService.getBusinesses(data)
             .then(response => {
                 console.log(response)
             })
             .catch(err => {
                 console.log(err)
             })
+    }
+
+    getGeoLocation(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(this.rtd, function(){
+                swal({
+                    type: "error",
+                    title: "Location not found",
+                    text: "Make sure you have https:// at the front of the url and allow location access. You can also use the Search functionality instead"
+                })
+            })
+        } else {
+            swal({
+                type: "error",
+                title: "Location not found",
+                text: "Make sure you have https:// at the front of the url and allow location access. You can also use the Search functionality instead"
+            })
+        }
     }
     render() {
         return (
