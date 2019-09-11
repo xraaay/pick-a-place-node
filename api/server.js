055d0a4575ba140f8604065088cb05da75961055
Ray <raymond.y.xie@gmail.com>
2019-09-11 13:45:44 -0700

    Working Yelp API request

--- api/server.js
+++ api/server.js
@@ -1,28 +1,40 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const request = require("request")
+const bodyParser = require('body-parser')
 
 dotenv.config()
 
 const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.listen(process.env.APP_PORT, () => console.log('listening on port ' + process.env.APP_PORT))
 
-app.get("/api/yelp", (req, res, next) => {
-    // Longitude, Latitude, Location, Radius, Price, OpenNow, Term, Limit, CurrentLocation
-    let yelpUrl = "https://api.yelp.com/v3/businesses/search"
+app.post("/api/yelp", (req, res, next) => {
+    let queryString = ""
+    
+    for(let x in req.body){
+        let add = `${x}=${req.body[x]}&`
+        queryString = queryString+add
+    }
+    
+    let yelpUrl = "https://api.yelp.com/v3/businesses/search?" + queryString
     let auth = {
         authorization: "Bearer " + process.env.YELP_API_KEY
     }
-
+    
     const options = {
         url: yelpUrl, 
         headers: auth
     }
-
+    
     function callback(err, response, body){
         res.send(body)
     }
-
+    
     request.get(options, callback)
 })
--- client/src/app/services/YelpService.js
+++ client/src/app/services/YelpService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const getBusinesses = data => {
     let url = "api/yelp"
     const config = {
-        method: "GET",
+        method: "POST",
         data: data
     }
     return axios(url, config)
