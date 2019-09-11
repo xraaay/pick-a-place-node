814b7a29c60def99a275b1126a335c9ca5f29567
Ray <raymond.y.xie@gmail.com>
2019-09-11 14:27:07 -0700

    working yelpcard

--- client/src/app/RollTheDice.jsx
+++ client/src/app/RollTheDice.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
-import * as yelpService from './services/YelpService'
+import * as yelpService from './services/yelpService'
 import swal from "sweetalert2"
+import { shuffleResults } from './services/reuseableFunctions'
+import {
+    Row,
+    Col,
+    Container,
+} from 'reactstrap'
+import YelpCard from './components/YelpCard'
+
 
 class RollTheDice extends Component {
-    componentDidMount(){
+    state = {
+        results: []
+    }
+
+    componentDidMount() {
         this.getGeoLocation()
     }
 
-    rtd(position){
+    rtd = (position) => {
         let data = {
             longitude: position.coords.longitude,
             latitude: position.coords.latitude,
@@ -19,35 +31,45 @@ class RollTheDice extends Component {
         }
         yelpService.getBusinesses(data)
             .then(response => {
-                console.log(response)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
-
-    getGeoLocation(){
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(this.rtd, function(){
-                swal({
-                    type: "error",
-                    title: "Location not found",
-                    text: "Make sure you have https:// at the front of the url and allow location access. You can also use the Search functionality instead"
+                let shuffle = shuffleResults(response.data.businesses)
+                let three = shuffle.slice(0, 3)
+                this.setState({
+                    results: three
                 })
             })
+            .catch(console.log)
+    }
+
+    getGeoLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.rtd, this.throwSwalError)
         } else {
-            swal({
-                type: "error",
-                title: "Location not found",
-                text: "Make sure you have https:// at the front of the url and allow location access. You can also use the Search functionality instead"
-            })
+            this.throwSwalError()
         }
+    }
+
+    throwSwalError() {
+        swal.fire({
+            type: "error",
+            title: "Location not found",
+            text: "Make sure you allow location access."
+        })
     }
     render() {
         return (
-            <div>
-                
-            </div>
+            <>
+                <Container>
+                    <Row style={{ marginTop: "100px" }}>
+                        {this.state.results && this.state.results.map(item => {
+                            return (
+                                <Col sm="4">
+                                    <YelpCard key={item.id} business={item}/>
+                                </Col>
+                            )
+                        })}
+                    </Row>
+                </Container>
+            </>
         )
     }
 }
--- client/src/app/components/YelpCard.jsx
+++ client/src/app/components/YelpCard.jsx
@@ -1,8 +1,56 @@
 import React from 'react';
-
+import {
+    Card,
+    CardImg,
+    CardText,
+    CardTitle,
+    Badge,
+} from 'reactstrap'
+import Rating from 'react-rating'
 class YelpCard extends React.Component {
-    render(){
+    render() {
+        let distance = Math.round(this.props.business.distance / 1600)
+            return (
+                <Card className="card-profile profile-bg">
+                    <a href={this.props.business.url}>
+                        <div style={{ height: "231.762px", display: 'flex', alignItems: 'center', overflow: 'hidden' }}>
+                            <CardImg src={this.props.business.image_url} style={{ flex: 'none', width: '100%' }} />
+                        </div>
+                    </a>
+                    <CardTitle tag="h3">{this.props.business.name}</CardTitle>
+                    <CardText className="text-center">
+                        <Rating
+                            readonly={true}
+                            initialRating={this.props.business.rating}
+                            emptySymbol={<i className="far fa-star"></i>}
+                            fullSymbol={<i className="fas fa-star"></i>}
+                        />
+                        <br></br>
+                        <span className="text-center">{this.props.business.is_closed
+                            ? <Badge color="danger" pill>Closed</Badge>
+                            : <Badge color="success" pill>Open</Badge>
+                        }
+                        </span>
+                        <br></br>
+                        <strong>{`${this.props.business.location.address1}, ${this.props.business.location.city}, ${this.props.business.location.zip_code}`}</strong>
+                        <br></br>
+                        <strong>{this.props.business.display_phone}</strong>
+                        <br></br>
+                        <CardText className="text-left ml-3 mt-3 mb-3">
 
+                            <strong>Distance:</strong> {distance <= 1 ? + distance + " mile" : distance + " miles"}
+                            <br></br>
+                            <strong>Price:</strong> {this.props.business.price}
+                            <br></br>
+                            <strong>Reviews:</strong> {this.props.business.review_count}
+                            <br></br>
+                            <strong>Categories:</strong> {this.props.business.categories.map((item, index) => {
+                                return (index ? ', ' : "") + item.title
+                            })}
+                        </CardText>
+                    </CardText>
+                </Card>
+            )
     }
 }
 
